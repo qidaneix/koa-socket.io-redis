88e69c0c20c2a14ad8b1511bb2e8f340c97dc1b6
xiaodabao <xiaojin1992@126.com>
2018-08-10 17:25:10 +0800

    nihao

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const Koa = require('koa');
 const nunjucks = require('nunjucks');
 const socketIO = require('socket.io');
@@ -5,7 +6,8 @@ const socketIO = require('socket.io');
 const { init, redis } = require('./init.js');
 
 const app = new Koa();
-const io = socketIO(app);
+const server = http.createServer(app.callback())
+const io = socketIO(server);
 
 app.use(async (ctx, next) => {
   await next();
@@ -13,12 +15,15 @@ app.use(async (ctx, next) => {
   ctx.response.body = nunjucks.render('index.html');
 });
 
-app.listen(3000);
-console.log('app started at port 3000...');
-init();
-
-
 io.on('connection', (socket) => {
   console.log('connect success!');
   socket.emit('hi', '你好哇！');
-})
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('app started at port 3000...');
+  init();
+});
